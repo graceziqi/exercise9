7b1ddb97db7be092148d19d34e7722cb62fc5ab6
Grace Zhang <nyd8pd@virginia.edu>
2023-07-31 10:55:26 -0400

    updated install

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+
+    name='exercise9',
+
+    version='1.0.0',
+
+    url='https://github.com/graceziqi/exercise9',
+
+    author='Grace Zhang',
+
+    author_email='ziqia58@gmail.com',
+
+    description='Description of my package',
+
+    packages=find_packages(),    
+
+)
